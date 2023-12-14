ba5c0ff27775a3016ebdc3aed3ae745e301609d9
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2023-12-14 17:56:30 +0200

    add button to header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -30,7 +30,7 @@ export default function Header () {
             <div className='header__logo-info'>
               <h1 className='header__title'><span className='header__title-name'>vl</span> marketing</h1>
               <h2 className='header__description'>creator | social media marketing | frontend developer</h2>
-              <button>
+              <button className='header__button'>
                 <a href='mailto:vlykhogodenko.pr@gmail.com' target='_blank' rel='noreferrer'>work with me</a>
               </button>
             </div>
